32a3b2320e26cc100c4bf0663b6fe5b846e31a15
Yoepee <kdy2975@naver.com>
2025-09-03 11:27:18 +0900

    초기 세팅

---
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -0,0 +1,14 @@
+package com.back.domain.post.post.entity;
+
+import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Post extends BaseEntity {
+    private String title;
+    private String content;
+}
