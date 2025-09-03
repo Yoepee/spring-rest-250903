4d7176dce4d8199fc8213c9d790dec63dcd55df4
Yoepee <kdy2975@naver.com>
2025-09-03 14:06:22 +0900

    RestController 작업

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -3,21 +3,19 @@ package com.back.domain.post.post.controller;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController // @Controller + @ResponseBody
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping("")
-    @ResponseBody
     public List<Post> getItems(){
         return postService.getList();
     }
