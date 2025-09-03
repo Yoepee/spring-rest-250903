37e90eb8289f0d44e7fc53aacde9b9fc989a95cd
Yoepee <kdy2975@naver.com>
2025-09-03 12:11:35 +0900

    PostComment 기능 추가

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,28 +1,43 @@
 package com.back.global.initData;
 
+import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
     private final PostService postService;
 
+    @Autowired
+    @Lazy
+    private BaseInitData self;
+
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
-            work1();
+            self.work1();
         };
     }
 
+    @Transactional
     public void work1() {
         if (postService.count() > 0) return;
 
-        postService.create("제목 1", "내용 1");
-        postService.create("제목 2", "내용 2");
+        Post post1 = postService.create("제목 1", "내용 1");
+        Post post2 =postService.create("제목 2", "내용 2");
         postService.create("제목 3", "내용 3");
+
+        post1.addPostComment("댓글 1");
+        post1.addPostComment("댓글 2");
+        post1.addPostComment("댓글 3");
+        post2.addPostComment("댓글 4");
+        post2.addPostComment("댓글 5");
     }
 }
