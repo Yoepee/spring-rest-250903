1c4425393e9286d40f5a0b08913010ea24a9609b
Yoepee <kdy2975@naver.com>
2025-09-04 12:24:33 +0900

    게시물 작성

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.post.controller;
 
 import com.back.domain.post.post.dto.PostDto;
+import com.back.domain.post.post.dto.PostWriteForm;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import com.back.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -37,6 +39,22 @@ public class ApiV1PostController {
         Post post = postService.getPost(id);
         postService.delete(post);
 
-        return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 삭제되었습니다.".formatted(id),
+                new PostDto(post)
+        );
+    }
+
+    @PostMapping("")
+    @Transactional
+    public RsData<PostDto> post(@Valid @RequestBody PostWriteForm form){
+        Post post = postService.create(form.title(), form.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
     }
 }
