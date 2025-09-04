76e51053b906b3a4f3febb35332a29f133848b2f
Yoepee <kdy2975@naver.com>
2025-09-04 15:37:21 +0900

    AOP(ResponseAspect)를 이용해서 응답 코드 변경

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -2,10 +2,20 @@ package com.back.global.rsData;
 
 public record RsData<T>(
         String resultCode,
-        String massage,
+        int statusCode,
+        String message,
         T data
 ) {
-    public RsData(String resultCode, String massage) {
-        this(resultCode, massage, null);
+    public RsData(String resultCode, String message) {
+        this(resultCode, message, null);
+    }
+
+    public RsData(String resultCode, String message, T data) {
+        this(
+                resultCode,
+                Integer.parseInt(resultCode.split("-", 2)[0]),
+                message,
+                data
+        );
     }
 }
