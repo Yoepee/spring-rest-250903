ace6b7c5128537d560d6e9cb691e122c7a0a1945
Yoepee <kdy2975@naver.com>
2025-09-05 11:40:56 +0900

    글 수정 테스트 추가

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,12 +32,29 @@ public class ApiV1PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("""
                                 {
-                                  "title": "제목new",
-                                  "content": "내용new"
+                                  "title": "제목 new",
+                                  "content": "내용 new"
                                 }
                                 """)
         ).andDo(print());
 
         resultActions.andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("글 수정")
+    void t2() throws Exception {
+        ResultActions resultActions = mvc.perform(
+                put("/api/v1/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "title": "제목 update",
+                                  "content": "내용 update"
+                                }
+                                """)
+        ).andDo(print());
+
+        resultActions.andExpect(status().isOk());
     }
 }
